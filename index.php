d5db39fb22264e15956b2957270cd3b6a3add54e
Ilja-Weber <ilja1989weber@googlemail.com>
2014-09-12 20:49:36 +0200

    test

--- index.php
+++ index.php
@@ -12,13 +12,7 @@
     </div>
 
     <script id='template' type='text/ractive'>
-        <h3>Hello {{name}}!</h3>
-        <p>You have <b>{{unread}}</b>/{{total}} unread messages.</p>
-
-        <div class='progress'>
-            <!-- You can use JavaScript expressions in templates! -->
-            <div style='width: {{ 100 * unread / total }}%;'></div>
-        </div>
+        <?php include 'templates/index.tpl'; ?>
     </script>
 
     <script src='http://cdn.ractivejs.org/latest/ractive.js'></script>
